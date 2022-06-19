d4d472da55a8f40f45d21058f2341b0f11502bef
Ben Goldstone <bgoldstone1@gmail.com>
2022-06-19 16:28:09 -0400

    updated App Link

--- frontend/src/Components/App.jsx
+++ frontend/src/Components/App.jsx
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 function App() {
   const [items, setItems] = useState([]);
   useEffect(() => {
@@ -21,17 +22,47 @@ function App() {
         </thead>
         <tbody>
           {items.map((item) => (
-            <tr
-              key={item.ID}
-              className="clickable"
-              onClick={() => (window.location = `/item/${item.ID}`)}
-              style={{ cursor: "pointer" }}
-            >
-              <td>{item.ID}</td>
-              <td className="">{item["name"]}</td>
-              <td className="px-3">{item["quantity"]}</td>
-              <td className="px-3">${item["price"].toFixed(2)}</td>
-              <td className="px-3">{item["description"]}</td>
+            <tr>
+              <td>
+                <Link
+                  to={`/item/${item.ID}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  {item.ID}
+                </Link>
+              </td>
+              <td className="">
+                <Link
+                  to={`/item/${item.ID}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  {item["name"]}
+                </Link>
+              </td>
+              <td className="px-3">
+                <Link
+                  to={`/item/${item.ID}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  {item["quantity"]}
+                </Link>
+              </td>
+              <td className="px-3">
+                <Link
+                  to={`/item/${item.ID}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  ${item["price"].toFixed(2)}
+                </Link>
+              </td>
+              <td className="px-3">
+                <Link
+                  to={`/item/${item.ID}`}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  {item["description"]}
+                </Link>
+              </td>
             </tr>
           ))}
         </tbody>
